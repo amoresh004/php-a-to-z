46f9e0ebc593be1e95cb8f376c98a385348298e5
amreshroy <amoreshray@gmail.com>
2023-01-10 00:23:42 +0600

    php constant variable

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
-$ami = "Amresh";
-$tumi = "Roy";
-$number = 123;
+// php variable ***
 
-echo "Hello {$ami} {$tumi} \n";
-echo 'Hello' ." ".$ami;
+// $ami = "Amresh";
+// $tumi = "Roy";
+// $number = 123;
+
+// echo "Hello {$ami} {$tumi} \n";
+// echo 'Hello' ." ".$ami;
+
+// php constant variable ***
+
+define('PI', 3.1416);
+$task = 'Read';
+echo $task ."\n";
+$task = 'Write';
+$constant = 'constant';
+echo $task ."\n";
+echo "Your value is = ".PI. "\n";
+echo constant ('PI')."\n";
+echo "Your value is = {$constant('PI')}";
