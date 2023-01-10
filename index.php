1cacbea4adbbc83aae1ce35c721bf33b3e0ca35d
amreshroy <amoreshray@gmail.com>
2023-01-11 00:12:53 +0600

    php output print

--- index.php
+++ index.php
@@ -10,12 +10,33 @@
 
 // php constant variable ***
 
-define('PI', 3.1416);
-$task = 'Read';
-echo $task ."\n";
-$task = 'Write';
-$constant = 'constant';
-echo $task ."\n";
-echo "Your value is = ".PI. "\n";
-echo constant ('PI')."\n";
-echo "Your value is = {$constant('PI')}";
+// define('PI', 3.1416);
+// $task = 'Read';
+// echo $task ."\n";
+// $task = 'Write';
+// $constant = 'constant';
+// echo $task ."\n";
+// echo "Your value is = ".PI. "\n";
+// echo constant ('PI')."\n";
+// echo "Your value is = {$constant('PI')}";
+
+// php output print ***
+/*
+integer int 
+Double / Float 
+Boolean 
+Null 
+String 
+Array 
+Object 
+Resource 
+*/
+$name = "Amresh";
+$result = "Passed";
+$exam = "HSC";
+// printf("You are $result");
+printf("Congratulation $name, You are $result $exam" ."\n");
+printf("Congratulation %s, You are %s %s",strtoupper($name), $result, strrev($exam) ."\n");
+echo "Congratulation ".$name.", You are ".$result. " ".$exam."\n";
+echo "Congratulation {$name}, You are {$result} {$exam} \n";
+var_dump($name);
